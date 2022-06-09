e306c8b9c5b5a9a7cd0897bcd848da8497240e6d
Hadrien <ketsapiwiq@protonmail.com>
2022-06-09 19:08:00 +0200

    pas de demo, si demo erreur Java 500

--- terraform/servers_providers/hcloud/main.tf
+++ terraform/servers_providers/hcloud/main.tf
@@ -9,7 +9,8 @@ provider "hcloud" {
 resource "hcloud_server" "bbb_server" {
   name  = "bbb-server"
   server_type = "cx41"
-  image = "ubuntu-16.04"
+#   image = "ubuntu-18.04"
+  image = "ubuntu-20.04"
   location = "nbg1"
   ssh_keys = [var.hcloud_ssh_key]
 }
